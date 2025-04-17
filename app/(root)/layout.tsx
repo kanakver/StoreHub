7b6b32f1bbe8cd99d9aac8c89a23712cf718ee29
Kanak verma <kanakv008@gmail.com>
2025-04-17 16:11:08 +0530

    added action dropdown functionalities and updated the home page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,7 +4,7 @@ import MobileNavigation from "@/components/MobileNavigation";
 import Header from "@/components/Header";
 import { getCurrentUser } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
-// import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/toaster";
 
 export const dynamic = "force-dynamic";
 
@@ -19,12 +19,11 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
 
       <section className="flex h-full flex-1 flex-col">
         <MobileNavigation {...currentUser} />
-  
         <Header userId={currentUser.$id} accountId={currentUser.accountId} />
         <div className="main-content">{children}</div>
       </section>
 
-      {/* <Toaster /> */}
+      <Toaster />
     </main>
   );
 };
